87b6b851692953924a4de847c0b42043e60c9650
Anu-khattri <anukc294@gmail.com>
2024-06-24 15:16:58 +0545

    Assignment 7

--- hoverefect/hovereffect.js
+++ hoverefect/hovereffect.js
@@ -1,13 +1,9 @@
+var box = document.getElementById("box");
 
+box.addEventListener("mouseenter", function () {
+    box.style.backgroundColor = "black";
+});
 
-document.addEventListener('DOMContentLoaded', function() {
-    var box = document.getElementById('box');
-
-    box.addEventListener('mouseenter', function() {
-        box.classList.add('box-hover');
-    });
-
-    box.addEventListener('mouseleave', function() {
-        box.classList.remove('box-hover');
-    });
+box.addEventListener("mouseleave", function () {
+    box.style.backgroundColor = "green";
 });
